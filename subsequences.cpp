35939fc486faf7f1a5fcde07a7628922b4bba223
Ashish Khulbey <65804342+ashish-khulbey@users.noreply.github.com>
2020-07-06 20:27:13 +0530

    Add files via upload

---
+++ subsequences.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void subsequence(char in[], char out[], int i, int j)
+{
+	if (in[i]=='\0') //Base Case
+	{
+		out[j]='\0';
+		cout<<out<<", ";
+		return;
+	}
+	//Including in[i]
+	out[j]=in[i];
+	subsequence(in, out, i+1, j+1);
+	//Excluding in[i]
+	out[j]='\0';
+	subsequence(in, out, i+1, j);
+}
+
+int main()
+{
+	char in[100]="abc";
+	char out[100];
+	subsequence(in, out, 0, 0);
+}
